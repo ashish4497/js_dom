5d02d1f3fe5605d8cd328c30d5a6133ebf75b3e0
ashish497 <ak8393267@gmail.com>
2020-03-03 17:25:13 +0530

    compleate Dom basic

---
+++ assignments/events/2. backgroundChange/main.js
@@ -0,0 +1,8 @@
+function generateRandomColor() {
+  var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor;
+}
+
+document.body.addEventListener("click", function () {
+  return document.body.style.backgroundColor = generateRandomColor();
+})
